8683f37d94c251fb67c0b6d7be3a783ddb895574
academyitwill <academyitwill@gmail.com>
2025-07-25 16:03:55 +0900

    ..

--- 01.HelloJava/src/myinfo.java
+++ 01.HelloJava/src/myinfo.java
@@ -7,7 +7,10 @@ public class myinfo {
 		 run             :  ctrl + f11
 		 line copy       :  ctrl + alt + down,up arrow 
 		 */
-		System.out.println();
+		System.out.println("나의정보");
+		System.out.println("난강사다~~~");
+		System.out.println("안물 안궁1~~~~");
+		System.out.println("안물 안궁2~~~~");
 
 	}
 
