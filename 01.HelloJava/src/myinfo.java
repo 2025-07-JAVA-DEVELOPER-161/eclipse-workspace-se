9d05903f7730ea243cc171d6e096287257c67fc8
academyitwill <academyitwill@gmail.com>
2025-07-25 15:56:07 +0900

    ..

---
+++ 01.HelloJava/src/myinfo.java
@@ -0,0 +1,14 @@
+
+public class myinfo {
+
+	public static void main(String[] args) {
+		/*
+		 content  assist :  ctrl + space
+		 run             :  ctrl + f11
+		 line copy       :  ctrl + alt + down,up arrow 
+		 */
+		System.out.println();
+
+	}
+
+}
