e281e8ebd80435e59e2ad0a020749f8c4b0ff466
academyitwill <academyitwill@gmail.com>
2025-07-25 16:07:18 +0900

    ..

--- 01.HelloJava/src/myinfo.java
+++ 01.HelloJava/src/myinfo.java
@@ -1,12 +1,13 @@
 
 public class myinfo {
-
 	public static void main(String[] args) {
 		/*
 		 content  assist :  ctrl + space
 		 run             :  ctrl + f11
 		 line copy       :  ctrl + alt + down,up arrow 
 		 */
+		
+		System.out.println("클래스이름 소문자는 관례에어긋남~~~");
 		System.out.println("나의정보");
 		System.out.println("난강사다~~~");
 		System.out.println("안물 안궁1~~~~");
