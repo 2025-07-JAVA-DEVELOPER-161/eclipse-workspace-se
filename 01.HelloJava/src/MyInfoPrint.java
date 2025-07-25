b2b2b4571163ccc10bc6ced3b016f822768d2b9c
academyitwill <academyitwill@gmail.com>
2025-07-25 16:58:33 +0900

    ..

---
+++ 01.HelloJava/src/MyInfoPrint.java
@@ -0,0 +1,17 @@
+/*
+  - 실행			: ctrl + f11
+  - content assist 	: ctrl + space
+  - 한라인 이동		: alt + arrow(down,up)
+  - 한라인 복사		: ctrl + alt + arrow(down,up)
+  */
+public class MyInfoPrint {
+
+	public static void main(String[] args) {
+		System.out.println("이름: 김경호");
+		System.out.println("별명: 보거스");
+		System.out.println("나이: 00");
+		System.out.println("키  : 180");
+
+	}
+
+}
